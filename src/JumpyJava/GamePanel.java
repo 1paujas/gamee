976a6cd8b289460feae38ce628696a93041eca8e
1paujas <1paujas@hdsb.ca>
2026-05-22 07:40:07 -0400

    Add files via upload

--- src/JumpyJava/GamePanel.java
+++ src/JumpyJava/GamePanel.java
@@ -5,27 +5,40 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class GamePanel extends JPanel implements Runnable, KeyListener {
 
+    Random rand = new Random();
+    int positionMin = 900;
+    int positionMax = 3800;
+
     // Create an instance of your Player
     Player player;
-    ArrayList<Block> blocks = new ArrayList<Block>();
+    public ArrayList<Block> blocks = new ArrayList<Block>();
     Thread gameThread;
+    boolean cooldown = true;
+
+    public void cooldownHandler() {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        scheduler.scheduleAtFixedRate(() -> {
+            cooldown = false;
+        }, 0, 2, TimeUnit.SECONDS);
+    }
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(800, 600));
         this.setBackground(Color.WHITE);
-        this.setFocusable(true); // REQUIRED to receive key presses
+        this.setFocusable(true);
         this.addKeyListener(this); // Listen for keys on this panel
 
         // Initialize player: x, y, width, height
         player = new Player(100, 300, 50, 50);
 
-        // Block
-        for (int i = 0; i < 5; i++) {
-            blocks.add(new Block(600, 400, 50, 50));
-        }
 
         // Start the game thread
         gameThread = new Thread(this);
@@ -34,6 +47,8 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 
     @Override
     public void run() {
+        cooldownHandler();
+
         while (true) {
             update();
             repaint();
@@ -46,11 +61,36 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
         }
     }
 
+    private void blockGen() {
+        if (!cooldown) {
+            if (blocks.size() <= 5) {
+                cooldown = true;
+                //int xPos = rand.nextInt((positionMax - positionMin) + 1) + positionMin;
+                int xPos = 900;
+                int size = rand.nextInt((100 - 50) + 50) + positionMin; // Temporary random size
+
+
+                boolean yPos = rand.nextBoolean();
+                if (yPos) {
+                    blocks.add(new Block(xPos, 510 - 250, 50, 300)); // Lower Block
+                } else {
+                    blocks.add(new Block(xPos, 0, 50, 300)); // Higher Block
+                }
+            }
+        }
+    }
+
     private void update() {
         player.update();
+
+        System.out.println(cooldown); //debug
+
+        blockGen();
+
         for (Block block : blocks) {
             block.update();
         }
+        blocks.removeIf(block -> block.x < -60);
     }
 
     @Override
@@ -62,7 +102,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
     private void draw(Graphics g) {
         player.draw(g);
         for (Block block : blocks) {
-            block.update();
+            block.draw(g);
         }
     }
 
